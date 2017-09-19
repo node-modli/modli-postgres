8fa894e26d835c0dcccc6f8280c6de977c390d06
Peter Svetlichny <peter.svetlichny@technologyadvice.com>
2017-09-19 14:01:05 -0700

    chore(test): update setup file; add dev deps

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,30 @@
 const chai = require('chai')
-global.assert = require('assert')
-global.should = require('should')
+const dirtyChai = require('dirty-chai')
+const chaiAsPromised = require('chai-as-promised')
+const mod = require('module')
+const path = require('path')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+const fixd = require('fixd')
+
+// Chai
+// ----------------------------------------
 global.expect = chai.expect
+chai.use(chaiAsPromised)
+chai.use(dirtyChai)
+chai.use(sinonChai)
+
+// Sinon
+// ----------------------------------------
+global.sinon = sinon
+
+// Fixd
+// ----------------------------------------
+global.fixd = fixd
+require('require-dir')(path.resolve(__dirname, 'fixtures'))
+
+// Node
+// ----------------------------------------
+// allow require() from project root
+process.env.NODE_PATH = path.join(process.cwd(), process.env.NODE_PATH || '')
+mod._initPaths()
